6364a69bd88d43d4cf2c10bb5d8ccb65df426573
Adam G <37425558+aglucky@users.noreply.github.com>
2022-03-05 02:25:46 -0500

    n

--- app.py
+++ app.py
@@ -1,42 +1,63 @@
 import pandas as pd
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder
-from st_aggrid.shared import GridUpdateMode
 import plotly.express as px
 from wordcloud import WordCloud
+from wordcloud import STOPWORDS
+import nltk
+import numpy as np
+from PIL import Image
 
 
 STREAMLIT_AGGRID_URL = "https://github.com/PablocFonseca/streamlit-aggrid"
 st.set_page_config(
-    layout="centered", page_icon="🖱️", page_title="Interactive table app"
+    layout="centered", page_icon="🖱️", page_title="Lexis Nexis Dashboard"
 )
-st.title("🖱️ Interactive table app")
+st.title("Russia After The Cold War")
+
+st.write("""After World War II, there was a period of geopolitical tension between the United States and Russia.
+        This dashboard allows you to explore the post-Cold War view of Russia.""")
+
+@st.cache(suppress_st_warning=True)
+def getMetaData():
+    countryDF = pd.read_csv("country.csv")
+    cityDF = pd.read_csv("city.csv")
+    companyDF = pd.read_csv("company.csv")
+    industryDF = pd.read_csv("industry.csv")
+    personDF = pd.read_csv("person.csv")
+    subjectDF = pd.read_csv("subject.csv")
+
+    metadata = {"country": countryDF,
+                "city": cityDF,
+                "company" : companyDF,
+                "industry" : industryDF,
+                "person" : personDF,
+                "subject" : subjectDF
+                }
+    return metadata
 
 
-countryDF = pd.read_csv("country.csv")
-cityDF = pd.read_csv("city.csv")
-companyDF = pd.read_csv("company.csv")
-industryDF = pd.read_csv("industry.csv")
-personDF = pd.read_csv("person.csv")
-subjectDF = pd.read_csv("subject.csv")
+@st.cache(suppress_st_warning=True)
+def getData():
+    df = pd.read_csv("core.csv", index_col=0)
+    df['text_token'] = df["content"].astype(str).apply(nltk.stem.SnowballStemmer("english").stem)
+    df.dropna()
+    return df
 
-metadata = {"country": countryDF,
-            "city": cityDF,
-            "company" : companyDF,
-            "industy" : industryDF,
-            "person" : personDF,
-            "subject" : subjectDF
-            }
+@st.cache(suppress_st_warning=True)
+def getStopwords():
+    return STOPWORDS.update(["said", "talk", "u", "now", "say","must","one","will","us","s"])
 
-df = pd.read_csv("core.csv", index_col=0)
 
-st.write("## News Data")
+df = getData()
+metadata = getMetaData()
+
+st.write("## News Data from 1995 to 2010")
 st.dataframe(df)
 
 st.write("## Reference Based Visualizations")
 
-st.write("### Chloropeth Reference Chart")
-fig = px.choropleth(countryDF, locations="value",
+st.write("### Who's mentioned the most in the data?")
+fig = px.choropleth(metadata['country'], locations="value",
                     color="count", 
                     hover_name="value", 
                     locationmode = 'country names',
@@ -51,19 +72,39 @@ pieChoose = st.selectbox(
      metadata.keys())
 
 fig = px.pie(metadata[pieChoose], values='count', names='value', 
-             title = f"News References Makeup by {pieChoose}",
+             title = f"News References Makeup by {pieChoose.capitalize()}",
             labels = {"count": "references",
                     "value":"subject"})
 
 fig.update_traces(textposition='inside')
 st.plotly_chart(fig, use_container_width=True)
 
+st.write("## Language Analysis")
+st.write("### Wordcloud by Year")
+yearChoice = st.slider('Select a Year', min_value = 1995,  max_value =2010, value = 1995)
+mask = np.array(Image.open('russia.png'))
+source = list(df.loc[df["year"] == yearChoice]['text_token'].values)
+long_string = ','.join(source)
+wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='red', stopwords=getStopwords(), width=5000, height=5000,)
+wordcloud.generate(long_string)
+st.image(wordcloud.to_image(), use_column_width = "auto" )
+
 st.write("## Sentiment Tracking")
 
-yearChoice = st.slider('Choose a year', 1900, 2022, 1)
-st.write("## Language Analyisis")
-source = list(df.loc[df["year"] == yearChoice]['content'].values)
-long_string = ','.join(source)
-wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
-wordcloud.generate(long_string)
-st.image(wordcloud.to_image())
+fig = px.histogram(df, x = "year")
+
+st.plotly_chart(fig, use_container_width=True)
+
+df["Positive_Count"] = df.loc[df["title_sentiment"] == 'POS', 'title_sentiment'].count()
+df["Negative_Count"] = df.loc[df["title_sentiment"] == 'NEG', 'title_sentiment'].count()
+df["Neutral_Count"] = df.loc[df["title_sentiment"] == 'NEU', 'title_sentiment'].count()
+fig = px.histogram(df, x="year", 
+             y=['Positive_Count','Negative_Count','Neutral_Count'], 
+             barmode = 'group',
+             title="Sentiment Labels by Year")
+
+st.plotly_chart(fig, use_container_width=True)
+
+fig = px.scatter_3d(df, x='year', y='title_score', z='word_count',
+                    title = "Country Subscription Price vs Library Size vs GDP per Capita")
+st.plotly_chart(fig, use_container_width=True)
